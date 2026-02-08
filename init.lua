bbd3dfb00c926d68d9d3abcdedaf25e537ff43c6
Olli <ollilain@gmail.com>
2026-02-08 15:27:06 +0200

    init config

---
+++ init.lua
@@ -0,0 +1,39 @@
+-- auto install vim-plug and plugins, if not found
+local data_dir = vim.fn.stdpath('data')
+if vim.fn.empty(vim.fn.glob(data_dir .. '/site/autoload/plug.vim')) == 1 then
+	vim.cmd('silent !curl -fLo ' .. data_dir .. '/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim')
+	vim.o.runtimepath = vim.o.runtimepath
+	vim.cmd('autocmd VimEnter * PlugInstall --sync | source $MYVIMRC')
+end
+
+local vim = vim
+local Plug = vim.fn['plug#']
+
+vim.g.start_time = vim.fn.reltime()
+vim.loader.enable() --  SPEEEEEEEEEEED 
+vim.call('plug#begin')
+
+Plug('catppuccin/nvim', { ['as'] = 'catppuccin' }) --colorscheme
+Plug('nvim-lualine/lualine.nvim') --statusline
+Plug('nvim-tree/nvim-web-devicons') --pretty icons
+Plug('folke/which-key.nvim') --mappings popup
+Plug('romgrk/barbar.nvim') --bufferline
+Plug('nvim-treesitter/nvim-treesitter') --improved syntax
+Plug('mfussenegger/nvim-lint') --async linter
+Plug('nvim-tree/nvim-tree.lua') --file explorer
+Plug('lewis6991/gitsigns.nvim') --git
+Plug('norcalli/nvim-colorizer.lua') --color highlight
+Plug('ibhagwan/fzf-lua') --fuzzy finder and grep
+Plug('MeanderingProgrammer/render-markdown.nvim') --render md inline
+
+vim.call('plug#end')
+
+-- mappings from external file
+require("configs.mappings")
+
+require("plugins.fzf-lua")
+require("plugins.nvim-tree")
+require("plugins.gitsigns")
+require("plugins.lualine")
+require("plugins.nvim-lint")
+require("plugins.render-markdown")
---
+++ lua/configs/mappings.lua
@@ -0,0 +1,19 @@
+-- mappings, including plugins
+
+local function map(m, k, v)
+	vim.keymap.set(m, k, v, { noremap = true, silent = true })
+end
+
+-- set leader
+map("", "<Space>", "<Nop>")
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+-- fzf
+map("n", "<leader>f", ":lua require('fzf-lua').files()<CR>") --search cwd
+map("n", "<leader>Fh", ":lua require('fzf-lua').files({ cwd = '~/' })<CR>") --search home
+map("n", "<leader>Fc", ":lua require('fzf-lua').files({ cwd = '~/.config' })<CR>") --search .config
+map("n", "<leader>Ff", ":lua require('fzf-lua').files({ cwd = '..' })<CR>") --search above
+
+-- misc
+map("n", "<leader>t", ":NvimTreeToggle<CR>") --open file explorer
