9956422a17b97f03fb3e09a32f3a279a3e93636f
grotsku <ollilain@gmail.com>
2026-02-14 15:13:58 +0200

    add buffer mappings

--- lua/configs/mappings.lua
+++ lua/configs/mappings.lua
@@ -9,6 +9,29 @@ map("", "<Space>", "<Nop>")
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
+-- buffers
+map("n", "<S-l>", ":bnext<CR>")
+map("n", "<S-h>", ":bprevious<CR>")
+map("n", "<leader>q", ":BufferClose<CR>")
+map("n", "<leader>Q", ":BufferClose!<CR>")
+map("n", "<leader>U", "::bufdo bd<CR>") --close all
+map('n', '<leader>vs', ':vsplit<CR>:bnext<CR>') --ver split + open next buffer
+
+-- buffer position nav + reorder
+map('n', '<AS-h>', '<Cmd>BufferMovePrevious<CR>')
+map('n', '<AS-l>', '<Cmd>BufferMoveNext<CR>')
+map('n', '<A-1>', '<Cmd>BufferGoto 1<CR>')
+map('n', '<A-2>', '<Cmd>BufferGoto 2<CR>')
+map('n', '<A-3>', '<Cmd>BufferGoto 3<CR>')
+map('n', '<A-4>', '<Cmd>BufferGoto 4<CR>')
+map('n', '<A-5>', '<Cmd>BufferGoto 5<CR>')
+map('n', '<A-6>', '<Cmd>BufferGoto 6<CR>')
+map('n', '<A-7>', '<Cmd>BufferGoto 7<CR>')
+map('n', '<A-8>', '<Cmd>BufferGoto 8<CR>')
+map('n', '<A-9>', '<Cmd>BufferGoto 9<CR>')
+map('n', '<A-0>', '<Cmd>BufferLast<CR>')
+map('n', '<A-p>', '<Cmd>BufferPin<CR>')
+
 -- fzf
 map("n", "<leader>f", ":lua require('fzf-lua').files()<CR>") --search cwd
 map("n", "<leader>Fh", ":lua require('fzf-lua').files({ cwd = '~/' })<CR>") --search home
